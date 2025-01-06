f2852eb7e7fae3bdd2dd89b67bc0748845f23fce
FCalic07 <97953488+FCalic07@users.noreply.github.com>
2025-01-06 13:30:27 +0100

    fixed issues with params

--- fubar-taxi-hci-project/src/app/about/[id]/page.tsx
+++ fubar-taxi-hci-project/src/app/about/[id]/page.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { ArrowLeft } from "lucide-react";
 import type { User } from "../page";
 import { Metadata } from "next";
+import { notFound } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Partner",
@@ -12,18 +13,24 @@ type TrusterPartnersProps = {
     params: { id: string };
 };
 
-async function getUsers(id: string): Promise<User[]> {
-    const response = await fetch(`${BASE_API_URL}/users/${id}`);
-    if (!response.ok) {
+async function getUser(id: string): Promise<User> {
+    const data = await fetch(`${BASE_API_URL}/users/${id}`);
+    if (!data.ok) {
       throw new Error("Failed to fetch users");
     }
-    return response.json();
+    return data.json();
 };
   
 export default async function PartnersPost({ params }: TrusterPartnersProps) {
-    const users = await getUsers(params.id);
+    const { id } = await params; //iz nekog razloga
+    const user = await getUser(id);
 
-    const { company, website } = users;
+    const { company, website } = user;
+
+    if (!id) {
+      notFound();
+    }
+
     return (
     <div
         className="relative min-h-screen bg-cover bg-center"
@@ -33,18 +40,18 @@ export default async function PartnersPost({ params }: TrusterPartnersProps) {
         <div className="absolute inset-0 bg-violet-950 opacity-60"></div>
         <div className="absolute inset-0 bg-black opacity-50"></div>
 
-        <main className="flex min-h-screen flex-col items-center p-10 z-20">
+        <main className="flex min-h-screen flex-col items-center p-10 z-10">
           <Link
             href="/about/"
-            className="inline-flex items-center text-gray-600 hover:text-gray-900 transition-colors duration-200 mb-6"
+            className="inline-flex items-center text-white-600 hover:text-gray-900 transition-colors duration-200 mb-6 z-10"
           >
             <ArrowLeft className="mr-2 h-4 w-4" /> Back to all partners
           </Link>
-          <h1 className="text-3xl md:text-4xl font-extrabold tracking-tight text-gray-900 mb-4">
+          <h1 className="text-3xl md:text-4xl font-extrabold tracking-tight text-white-900 mb-4 z-10">
             {company.name}
           </h1>
-          <p>{website}</p>
-      </main>
+          <p className="z-10">{website}</p>
+        </main>
       
     </div>
     );
