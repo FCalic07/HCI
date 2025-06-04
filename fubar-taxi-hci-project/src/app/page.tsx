7e5db5548688340bd8854eb8f9aeae5b098d6938
FCalic07 <fcalic00@fesb.hr>
2025-06-04 14:56:16 +0200

    Update hero section

--- fubar-taxi-hci-project/src/app/page.tsx
+++ fubar-taxi-hci-project/src/app/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
       {/* Hero Section */}
       <div
         id="hero"
-        className="relative "
+        className="relative md:min-h-screen h-[880px] max-w-screen"
         style={{ contentVisibility: "auto" }}
       >
         <Image
